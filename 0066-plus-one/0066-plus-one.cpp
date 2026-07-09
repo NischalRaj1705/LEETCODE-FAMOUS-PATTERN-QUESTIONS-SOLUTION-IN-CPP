70551af97f5b0b6ab16ed7bed251690c1dad8c3a
NischalRaj1705 <nischalraj1705@gmail.com>
2026-07-10 01:37:41 +0530

    Time: 0 ms (100%), Space: 11.2 MB (97.49%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> ans;
+        int carry = 1;
+        int sum;
+        int n= digits.size() - 1;
+        for (int i = digits.size() - 1; i>= 0; i--) {
+            sum = digits[i] + carry;
+            ans.push_back(sum % 10);
+            carry = sum / 10;
+        }
+        if (carry)
+            ans.push_back(carry);
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
