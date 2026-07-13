51272719cf01c2107cc488759077ce17aee9d7a7
NischalRaj1705 <nischalraj1705@gmail.com>
2026-07-13 23:14:19 +0530

    Time: 0 ms (100%), Space: 7.7 MB (54.85%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -3,10 +3,10 @@ public:
     bool judgeSquareSum(int c) {
         
         int root = sqrt(c);
-        int arr[root + 1];
-        for (int i = 0; i < root + 1; i++) {
-            arr[i] = i;
-        }
+        // int arr[root + 1];
+        // for (int i = 0; i < root + 1; i++) {
+        //     arr[i] = i;
+        // }
         int l = 0;
         int r = root;
         while (l <= r) {
